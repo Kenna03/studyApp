376ddfbcd11e9f1af32740a671ee31c46114b0d3
Kenna03 <mikennarehelsutherl@cmail.carleton.ca>
2023-11-05 18:35:48 -0500

    added a template for the rest of assignment details

--- studyapp/urls.py
+++ studyapp/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('create_assignment/', views.create_assignment, name='create_assignment'),
-    path('/details/<int:id>', views.details, name='details'),
+    path('details/<int:id>', views.details, name='details'),
 ]
