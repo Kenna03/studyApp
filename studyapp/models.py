696deead655bbecc6e481b11b2725e6559af34a9
Kenna03 <mikennarehelsutherl@cmail.carleton.ca>
2023-11-05 15:12:12 -0500

    fixed unresolved reference error

--- studyapp/models.py
+++ studyapp/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 
+
 # Create your models here.
 class Assignment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=50, null=False, blank=False)
-    course = models.CharField(max_length=100, null=False, blank=False)
+    title = models.CharField(max_length=50, default="")
+    course = models.CharField(max_length=100, default="")
+    description = models.CharField(max_length=255, default="")
     dueDate = models.DateTimeField()
     completed = models.BooleanField(default=False)
 
--- studyapp/views.py
+++ studyapp/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
 from .models import Assignment
 from django.shortcuts import render, redirect
-from .forms import AssignmentForm
 from django.views.decorators.http import require_POST
-from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
-
+from .form import AssignmentForm
 
 # Create your views here.
+@login_required()
 def index(request):
-    assignments = Assignment.objects.all()
-    return render(request, 'index.html', {'assignment': assignments})
+    assignments = Assignment.objects.filter(user=request.user)
+    form = AssignmentForm()
+    return render(request, 'index.html', {'assignments': assignments, 'form': form})
+
+@login_required
+@require_POST  # Ensure that only POST requests can access this view
+def create_assignment(request):
+    form = AssignmentForm(request.POST)
+    print('preparing to save')
+    if form.is_valid():
+        assignment = form.save(commit=False)
+        assignment.user = request.user
+        assignment.save()
+        print('saved')
+    return redirect('index')
