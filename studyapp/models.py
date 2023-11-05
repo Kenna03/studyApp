d6a751e5e0ab51f8027d4b38ceb22428f08ca5d4
Kenna03 <mikennarehelsutherl@cmail.carleton.ca>
2023-11-05 13:43:25 -0500

    added a database

--- studyapp/models.py
+++ studyapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Assignment(models.Model):
+    title: models.CharField(max_length=50)
+    course: models.CharField(max_length=100)
+    description: models.CharField(max_length=255)
+    dueDate: models.DateTimeField()
+
+    def __str__(self):
+        return self.title
--- studyapp/views.py
+++ studyapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Assignment
+
 
 # Create your views here.
+def index(request):
+    assignments = Assignment.objects.all()
+    return render(request, 'index.html', {'assignment': assignments})
